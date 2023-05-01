cfe718a32f1acee41e3674968c78929ec1320f39
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-01 12:58:39 +0200

    submit e.target

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const formEl = document.querySelector('form')
 
 formEl.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    const company = e.target.elements.company
+
+    console.log(company.value);
 })
