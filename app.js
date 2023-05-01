5f98290b98248ab9f7e7f253c88161ca891d355f
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-01 12:48:15 +0200

    submit

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const formEl = document.querySelector('form')
 
-formEl.addEventListener('reset', function(e) {
-    const userConfirm = confirm('Are you sure?')
-    if (!userConfirm) {
-        e.preventDefault();
-    }
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault();
 })
