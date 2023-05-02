33964be7f464da65babb4c159b4f1e2ee691932c
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-02 10:40:07 +0200

    readAsDataURL

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const fileEl = document.querySelector('input')
 
-fileEl.addEventListener('change', showInfoFile)
-
-function showInfoFile(e) {
+fileEl.addEventListener('change', readFile)
+function readFile(e) {
     const file = e.target.files[0]
-    console.log(
-        file.name,
-        file.size,
-        file.type,
-    );
+    console.log(e.target, 'e.target');
+    console.log(e.target.files[0], 'e.target.files[0]');
+
+    if (file && file.type.includes('image')) {
+        const reader = new FileReader()
+
+        console.log(reader);
+        
+        reader.onload = function (readerEvent) {
+            const newImg = document.createElement('img')
+            newImg.src = readerEvent.target.result
+
+            console.log(newImg.src);
+            console.log(readerEvent);
+            console.log(readerEvent.target);
+            console.log(readerEvent.target.result);
+
+            document.body.appendChild(newImg)
+        }
+
+        reader.readAsDataURL(file)
+    }
 }
 
 
