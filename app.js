704d380af4c0f89dd266a20a3f562fef50444c91
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-02 13:53:56 +0200

    select 1

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-const colorEl = document.querySelector('input')
-colorEl.addEventListener('change', changeBGColor)
+const selectEl = document.querySelector('select')
+selectEl.addEventListener('change', showUser)
 
-function changeBGColor(e) {
-    const body = document.body
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.value);
-    const color = e.target.value
-
-    body.style.backgroundColor = color
-    body.style.color = invertColor(color)
-}
-
-function invertColor(hex) {
-    console.log(hex);
-    let color = '#'
-    for (let i = 1; i < 6; i = i + 2) {
-        const dec = 255 - parseInt(hex.substr(i, 2), 16)
-        console.log(dec);
-        color += (dec < 16 ? '0' : '') + dec.toString(16)
-        console.log(color);
-    }
-    return color
+function showUser(e) {
+    console.log(
+        e.target.value,
+        e.target.selectedIndex,
+        selectEl.selectedOptions,
+    );
 }
