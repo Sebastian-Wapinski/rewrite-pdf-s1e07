58fa2c1fa65155d405188b12a55218f810bc29d5
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-02 11:58:13 +0200

    radio 1

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-const pElement = document.querySelector('p')
-const fileEl = document.querySelector('input')
+const sizeList = document.querySelectorAll('[type="radio"]')
 
-fileEl.addEventListener('change', readFile)
-function readFile(e) {
-    const file = e.target.files[0]
-    console.log(e);
-    console.log(e.target, 'e.target');
-    console.log(e.target.files[0], 'e.target.files[0]');
+sizeList.forEach(function (element) {
+    element.addEventListener('change', showValue)
+})
 
-    const checkE = e
-    console.log(checkE, 'checkE');
-
-    if (file && file.type.includes('text')) {
-        const reader = new FileReader()
-
-        console.log(checkE, 'checkE inside');
-        console.log(reader);
-        
-        reader.onload = function (readerEvent) {
-            const content = readerEvent.target.result
-            pElement.textContent = content
-
-            console.log(checkE, 'checkE inside inside');
-            console.log(content);
-            console.log(readerEvent);
-            console.log(readerEvent.target);
-            console.log(readerEvent.target.result);
-            
-        }
-        reader.readAsText(file, 'UTF-8')
-        
-    } else {
-        alert('Wybierz plik tekstowy')
-    }
+function showValue(e) {
+    console.log(e.target.value);
 }
 
 
