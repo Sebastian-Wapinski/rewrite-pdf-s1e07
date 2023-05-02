8e1ab3127822356082c67b1626113252c39da609
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-02 15:18:01 +0200

    check @

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const textEl = document.querySelector('textarea')
-textEl.addEventListener('change', showText)
-textEl.addEventListener('input', prepareHeight)
+const spanEl = document.querySelector('span')
+const inputEl= document.querySelector('input')
 
-function showText(e) {
-    console.log(e.target.value);
-}
+inputEl.addEventListener('input', checkEmail)
 
-function prepareHeight(e) {
-    const item = e.target
-    // console.log(e);
+function checkEmail(e) {
+    const self = e.target
+    const email = self.value
 
-    if (item.scrollHeight > item.offsetHeight) {
-        item.style.height = item.scrollHeight + 'px'
+    if (!email.includes('@')) {
+        self.style.border = '1px solid red'
+        spanEl.innerText = 'Where is @'
+    } else {
+        self.style.border = '1px solid green'
+        spanEl.innerText = ''
     }
 }
