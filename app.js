04c58725a5381fe695456ecaab0ff011cfb0f197
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-02 13:58:29 +0200

    select 2

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ selectEl.addEventListener('change', showUser)
 
 function showUser(e) {
     console.log(
+        e,
+        e.target,
         e.target.value,
         e.target.selectedIndex,
         selectEl.selectedOptions,
+        e.target.innerText
     );
 }
